538ced4367fb56d85a57f2b207fa40d65f9024a6
Jamshed Hossan <jamshedhossan9@gmail.com>
2024-01-20 21:43:06 +0600

    github page try 5

--- vite-doc.config.js
+++ vite-doc.config.js
@@ -16,19 +16,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: '/vue-hour-range-picker/',
   build: {
-    lib: {
-      // src/indext.ts is where we have exported the component(s)
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "VueHourRangePicker",
-      // the name of the output files when the build is run
-      fileName: "hour-range-picker",
-    },
+    outDir: 'doc',
     rollupOptions: {
-        
+        input: {
+            main: resolve(__dirname, 'index.html'),
+          },
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue"],
+    //   external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
