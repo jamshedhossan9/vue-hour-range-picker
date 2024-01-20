906b46ac8f4b08e152974f3432d32e9551afe872
Jamshed Hossan <jamshedhossan9@gmail.com>
2024-01-20 19:29:53 +0600

    githhub page update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { resolve } from "path"
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    
+    vue()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
